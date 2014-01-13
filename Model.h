b133b5738218047c1e049677ecc91484a96baafb
LandReagan <landry.gagne@wanadoo.fr>
2014-01-13 14:19:29 +0100

    Settling of MVC
    	new file:   Model.h
    	new file:   main.cpp

---
+++ Model.h
@@ -0,0 +1,19 @@
+/*
+
+This class implements Model in the Model-View-Controller pattern
+As per definition, it should inherit from Observable class in Observer pattern
+
+*/
+
+#ifndef MODEL_H_INCLUDED
+#define MODEL_H_INCLUDED
+
+class Model
+{
+   public:
+
+   private:
+
+};
+
+#endif // MODEL_H_INCLUDED
